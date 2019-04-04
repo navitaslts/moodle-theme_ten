43f9819b26edd720e90c64b03d9a4115ea5c80c3
Nicosoft <nicolas.jourdain@gmail.com>
2019-04-04 11:59:58 +1100

    WIP

--- lang/en/theme_ten.php
+++ lang/en/theme_ten.php
@@ -25,48 +25,11 @@
 // This line protects the file from being accessed by a URL directly.
 defined('MOODLE_INTERNAL') || die();
 
-// The name of the second tab in the theme settings.
-$string['advancedsettings'] = 'Advanced settings';
-// The backgrounds tab name.
-$string['backgrounds'] = 'Backgrounds';
-// The brand colour setting.
-$string['ncmbrandcolor'] = 'Brand colour';
-// The brand colour setting description.
-$string['ncmbrandcolor_desc'] = 'The accent colour.';
-// The key text colour setting.
-$string['ncmkeytxtcolor'] = 'Key text colour';
-// The key text colour setting description.
-$string['ncmkeytxtcolor_desc'] = 'The key text colour.';
-// The key text colour setting.
-$string['ncmlinkcolor'] = 'Link colour';
-// The key text colour setting description.
-$string['ncmlinkcolor_desc'] = 'The Link colour.';
 // A description shown in the admin theme selector.
 $string['choosereadme'] = 'Theme TEN is a child theme of NCMBoost.';
 // Name of the settings pages.
 $string['configtitle'] = 'TEN settings';
-// Background image for dashboard page.
-$string['dashboardbackgroundimage'] = 'Dashboard page background image';
-// Background image for dashboard page.
-$string['dashboardbackgroundimage_desc'] = 'An image that will be stretched to fill the background of the dashboard page.';
-// Background image for default page.
-$string['defaultbackgroundimage'] = 'Default page background image';
-// Background image for default page.
-$string['defaultbackgroundimage_desc'] = 'An image that will be stretched to fill the background of all pages without a more specific background image.';
-// Background image for front page.
-$string['frontpagebackgroundimage'] = 'Front page background image';
-// Background image for front page.
-$string['frontpagebackgroundimage_desc'] = 'An image that will be stretched to fill the background of the front page.';
-// Name of the first settings tab.
 $string['generalsettings'] = 'General settings';
-// Background image for incourse page.
-$string['incoursebackgroundimage'] = 'Course page background image';
-// Background image for incourse page.
-$string['incoursebackgroundimage_desc'] = 'An image that will be stretched to fill the background of course pages.';
-// Background image for login page.
-$string['loginbackgroundimage'] = 'Login page background image';
-// Background image for login page.
-$string['loginbackgroundimage_desc'] = 'An image that will be stretched to fill the background of the login page.';
 // The name of our plugin.
 $string['pluginname'] = 'TEN';
 // ...Login Page Type.
@@ -80,4 +43,3 @@ $string['samlauthfirst'] = 'SAML Auth First';
 $string['tenlogin'] = 'TEN Login';
 $string['manualauthoff'] = 'Manual Auth OFF';
 $string['manualaccounttitle'] = 'Manual Accounts Only';
-
