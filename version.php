487027e2b42b825107aac9eb21e026f8157af001
Nicosoft <nicolas.jourdain@gmail.com>
2019-04-08 16:06:17 +1000

    Clean up

--- version.php
+++ version.php
@@ -26,7 +26,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 // This is the version of the plugin.
-$plugin->version = 2019040808;
+$plugin->version = 2019040809;
 
 // This is the version of Moodle this plugin requires.
 $plugin->requires = 2018050800;
