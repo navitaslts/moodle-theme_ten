2a9dd8f011a66355d892288761acfcd684fa9278
Nicosoft <nicolas.jourdain@gmail.com>
2019-04-12 12:14:41 +1000

    update text

--- lang/en/theme_ten.php
+++ lang/en/theme_ten.php
@@ -49,3 +49,24 @@ $string['privacy:drawernavclosed'] = 'The current preference for the navigation 
 $string['privacy:drawernavopen'] = 'The current preference for the navigation drawer is open.';
 // We need to include a lang string for each block region.
 $string['region-side-pre'] = 'Right';
+// Mustache Templates String
+$string['welcometitle'] = 'Welcome to TEN Moodle';
+$string['next'] = 'NEXT';
+$string['btn-startagain'] = 'START AGAIN';
+$string['btn-help'] = 'HELP';
+$string['btn-manual'] = 'Manual Login';
+$string['btn-navitas'] = 'NAVITAS Login';
+
+$string['step1-intro'] = 'Please enter your work email (the one you use to login to other Navitas services eg. jane.citizen@navitas.com).';
+$string['step1-placeholder'] = 'Your work email address';
+$string['step2-manual-intro'] = 'You have registered for a course before, and received your login details via an email from us afterwards? Please use these login credentials.';
+$string['step-help-alert'] = 'Looks like we don’t recognise your work email address or you need help?';
+$string['step-help-intro'] = 'To make sure you get your account working before the course starts, please click the button below and we will get in touch with you.';
+$string['step-help-btn'] = 'RAISE SERVICE DESK TICKET';
+
+$string['step2-navitas-intro'] = 'Hi! Your email address is from a Navitas partner college. You will need to use your navitas.com email (eg. jane.citizen@navitas.com), or let us know if you don’t have one.';
+$string['step2-navitas-placeholder'] = 'jane.citizen@navitas.com';
+$string['step2-navitas-nonavitas'] = 'I don\'t have an @navitas.com email address';
+
+
+
--- version.php
+++ version.php
@@ -26,7 +26,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 // This is the version of the plugin.
-$plugin->version = 2019040809;
+$plugin->version = 2019041202;
 
 // This is the version of Moodle this plugin requires.
 $plugin->requires = 2018050800;
